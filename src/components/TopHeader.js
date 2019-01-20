b773c36ced303dc5f3b316be81a3ec5d55c25d2f
Olivier ESUKA <oesukam@gmail.com>
2019-01-20 21:28:47 +0200

    Add bulma

---
+++ src/components/TopHeader.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class TopHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileMenu: false,
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+
+      </nav>
+    )
+  }
+};

bae69514e2e72bcc2026937fac5d9e6d2e62d334
Olivier ESUKA <oesukam@gmail.com>
2019-04-22 13:23:15 +0200

    Implement the nav menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import 'bulma/css/bulma.css'
-import './App.css';
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "bulma/css/bulma.css";
+import "./App.css";
+import TopHeader from "./components/TopHeader/TopHeader";
+import Home from "./components/Home/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        
-      </div>
+      <Router>
+        <TopHeader />
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore } from 'redux';
-import reducers from './reducers/index';
+import { createStore } from "redux";
+import reducers from "./reducers/index";
 
 const store = createStore(reducers);
 
-console.log(store.getState(), 'pppp');
+console.log(store.getState(), "pppp");
 
 // Subscription
 
 store.subscribe(() => {
-  console.log('[subscription]', store.getState());
-})
+  console.log("[subscription]", store.getState());
+});
 
 export default store;
